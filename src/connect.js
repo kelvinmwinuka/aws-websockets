ab13a88ab63d3febaaea613b8d400983e2b4ea94
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2022-10-29 08:03:47 +0800

    Added DynamoDB to store current active websocket connections

--- src/connect.js
+++ src/connect.js
@@ -1,11 +1,32 @@
 "use strict";
 
-module.exports.handler = async (event) => {
+const {
+  DynamoDB,
+  PutItemCommand
+} = require("@aws-sdk/client-dynamodb");
+
+const {
+  REGION,
+  CONNECTION_TABLE
+} = process.env;
+
+module.exports.handler = async (event, context) => {
   console.log(event);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Connection established!"
-      })
-    };
+
+  const dynamoDBClient = new DynamoDB({ region: REGION });
+  const putItemCommand = new PutItemCommand({
+    TableName: CONNECTION_TABLE,
+    Item: {
+      Username: { S: event.queryStringParameters.username },
+      ConnectionId: { S: event.requestContext.connectionId }
+    }
+  });
+  await dynamoDBClient.send(putItemCommand);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Connection established!"
+    })
+  };
 }
--- src/disconnect.js
+++ src/disconnect.js
@@ -1,7 +1,27 @@
 "use strict";
 
+const {
+  DynamoDB,
+  DeleteItemCommand
+} = require("@aws-sdk/client-dynamodb");
+
+const {
+  REGION,
+  CONNECTION_TABLE
+} = process.env;
+
 module.exports.handler = async (event) => {
   console.log(event);
+
+  const dynamoDBClient = new DynamoDB({ region: REGION });
+  const deleteItemCommand = new DeleteItemCommand({
+    TableName: CONNECTION_TABLE,
+    Key: {
+      ConnectionId: { S: event.requestContext.connectionId }
+    }
+  });
+  await dynamoDBClient.send(deleteItemCommand);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Connection ended!" })
