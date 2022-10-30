a883c3716f6608b17aa4ac698c69baed45dcd802
Kelvin Clement Mwinuka <kelvinmwinuka@hotmail.co.uk>
2022-10-31 03:56:17 +0800

    Delete DynamoDB record when user disconnects

--- src/disconnect.js
+++ src/disconnect.js
@@ -15,10 +15,23 @@ module.exports.handler = async (event) => {
   console.log(event);
 
   const dynamoDBClient = new DynamoDB({ region: REGION });
+
+  // Retrieve the connection using the connectionId
+  const queryCommand = new QueryCommand({
+    TableName: CONNECTION_TABLE,
+    IndexName: "ConnectionIdIndex",
+    KeyConditionExpression: "ConnectionId = :a",
+    ExpressionAttributeValues: {
+      ":a": { S: event.requestContext.connectionId }
+    }
+  })
+  const queryResult = await dynamoDBClient.send(queryCommand);
+  const connectionRecord = queryResult.Items[0];
+
   const deleteItemCommand = new DeleteItemCommand({
     TableName: CONNECTION_TABLE,
     Key: {
-      ConnectionId: { S: event.requestContext.connectionId }
+      Username: { S: connectionRecord.Username.S }
     }
   });
   await dynamoDBClient.send(deleteItemCommand);
